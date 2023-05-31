7775f79fc2756b6e02f2a88e278168ff9e08900a
Ahmed Samir Sakr <89707420+ahmedsamir010@users.noreply.github.com>
2023-06-01 02:39:15 +0300

    Add files via upload

---
+++ 9. Palindrome Number.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        string str = x.ToString();
+        string reverse = new string(str.Reverse().ToArray());
+
+        return reverse.Equals(str);
+
+
+    }
+}
