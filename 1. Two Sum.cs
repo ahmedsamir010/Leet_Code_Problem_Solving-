6e8495eac02c8e211d76cdc7b4d0b4672693e928
Ahmed Samir Sakr <89707420+ahmedsamir010@users.noreply.github.com>
2023-06-03 11:21:26 +0300

    Create 1. Two Sum.cs

---
+++ 1. Two Sum.cs
@@ -0,0 +1,20 @@
+// Author : Ahmed Samir 
+// Link Problem : https://leetcode.com/problems/two-sum/
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] arr = new int[2]; // Initialize the array with a size of 2
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        arr[0] = i;
+                        arr[1] = j;
+                      
+                    }
+                }
+            }
+            return arr; // Return the array
+    }
+}
