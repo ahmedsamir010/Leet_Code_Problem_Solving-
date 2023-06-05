b5bd4345fc4193c590da397315e15e3bab7c522e
Ahmed Samir Sakr <89707420+ahmedsamir010@users.noreply.github.com>
2023-06-05 23:54:36 +0300

    Create 1662. Check If Two String Arrays are Equivalent.cs

---
+++ 1662. Check If Two String Arrays are Equivalent.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
+           var c1 = "";
+            var c2="";
+            for (int i = 0; i < word1.Length; i++)
+            {
+                c1 += word1[i];
+            }
+            for (int i = 0; i < word2.Length; i++)
+            {
+                c2 += word2[i];
+            }
+
+
+
+
+            return c1==c2;
+
+    }
+}
